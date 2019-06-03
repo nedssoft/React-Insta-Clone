e984d1910364284261f82d19fcf94bb77e5393d0
Orie Chinedu <oriechinedu@gmail.com>
2019-06-03 14:53:14 +0100

    Complete SearchBar Component

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './SearchBar.css'
+import Logo from './Logo'
+import SearchInput from './SearchInput'
+import SearchIcons from './SearchIcons'
 
 export default function searchBar() {
   return (
     <div className="search-bar">
-    
+      <Logo />
+      <SearchInput />
+      <SearchIcons />
   </div>
   )
 }
---
+++ instagram-app/src/components/SearchBar/SearchIcons.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './SearchBar.css'
+
+export default function logo() {
+  return (
+  
+    <ul className="icons">
+      <li><i className="fas fa-external-link-square-alt"></i></li>
+      <li><i className="far fa-heart"></i></li>
+      <li><i className="far fa-user"></i></li>
+    </ul>
+  )
+}
