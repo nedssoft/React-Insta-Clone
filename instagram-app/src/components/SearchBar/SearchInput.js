ee32987bc4f4d3a0781fa641eeb0a07ffa9b0567
Orie Chinedu <oriechinedu@gmail.com>
2019-06-05 18:24:11 +0100

    deploy

--- instagram-app/src/components/SearchBar/SearchInput.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './SearchBar.css'
-
-export default function searchInput({searchHandler}) {
-  return (
-    <input type="search" className="search-input" placeholder="search" onChange={searchHandler} />
-  );
-}
