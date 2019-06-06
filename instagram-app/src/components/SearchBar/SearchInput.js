fbc35cefff6414b0bc7f140f98b9e020803fa438
Orie Chinedu <oriechinedu@gmail.com>
2019-06-06 13:31:54 +0100

    Convert Search inout component to a styled component

--- instagram-app/src/components/SearchBar/SearchInput.js
+++ instagram-app/src/components/SearchBar/SearchInput.js
@@ -1,8 +1,24 @@
 import React from 'react'
-import './SearchBar.css'
+import styled from 'styled-components';
 
+const SearchInput = styled.input`
+  width           : 200px;
+  outline         : none;
+  border-radius   : 3px;
+  text-align      : center;
+  padding         : 0.5rem;
+  border          : #ffffff;
+  font-size       : 1.6rem;
+  background-color: rgba(243, 243, 243, 0.383);
+  border          : 1px solid #ccc;
+  @media (max-width: 500px) {
+      width: 100%;
+      order: 1;
+      margin-bottom: 2rem;
+  }
+`
 export default function searchInput({searchHandler}) {
   return (
-    <input type="search" className="search-input" placeholder="search" onChange={searchHandler} />
+    <SearchInput type="search" className="search-input" placeholder="search" onChange={searchHandler} />
   );
 }
